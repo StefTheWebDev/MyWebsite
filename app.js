7df4a2f50f50cfddc0d7e81cbb4e0c32bdf7af11
Stefan Dixon <sjdixon1997@yahoo.co.nz>
2019-02-07 14:43:41 +1300

    fixed

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/loginapp');
+mongoose.connect('mongodb://Stefandixon15:Ammdesspr94@ds125225.mlab.com:25225/stefandixon');
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
